10640bcd243e9bd204738d2d7823e50aba435b78
PP BackEnd <pvphat25@gmail.com>
2022-08-04 12:35:12 -0400

    2nd ready for deployment

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://locallibrary:express-locallibrary-tutorial@cluster0.2jlhjs5.mongodb.net/?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://locallibrary:express-locallibrary-tutorial@cluster0.2jlhjs5.mongodb.net/?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
